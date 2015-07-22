afccfd3382832fa0eb479ced7ec8125b34024b95
Roku <eric404dev@gmail.com>
2015-07-22 03:20:21 +0200

    index.php
    The index page which displays a simple form asking for the user to input a Twitch username.

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Twitch Status.</title>
+</head>
+<body>
+Enter the username of the streamer you want to check the status of!
+<br> <form action='action.php' method='post'> <p>Username: <input type='text' name='stream' /></p> <p><input type='submit' /></p> </form>
+</body>
+</html>
